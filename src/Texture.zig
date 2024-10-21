44d207f5e9abd3021c72a47174e22c95939955aa
Paul Davies <lagdotcom@gmail.com>
2024-10-21 10:23:29 +0100

    adding more guff to Texture

--- src/Texture.zig
+++ src/Texture.zig
@@ -42,14 +42,61 @@ pub const Texture = struct {
         sdl.SDL_DestroyTexture(self.texture);
     }
 
-    pub fn render(self: *Texture, x: f32, y: f32) void {
-        var dest = sdl.SDL_FRect{
+    pub fn set_color(self: *Texture, r: u8, g: u8, b: u8) void {
+        _ = sdl.SDL_SetTextureColorMod(self.texture, r, g, b);
+    }
+
+    pub fn set_alpha(self: *Texture, a: u8) void {
+        _ = sdl.SDL_SetTextureAlphaMod(self.texture, a);
+    }
+
+    pub fn set_blending(self: *Texture, mode: sdl.SDL_BlendMode) void {
+        _ = sdl.SDL_SetTextureBlendMode(self.texture, mode);
+    }
+
+    inline fn get_destination_rect(self: *Texture, x: f32, y: f32) sdl.SDL_FRect {
+        return sdl.SDL_FRect{
             .x = x,
             .y = y,
             .w = @floatFromInt(self.width),
             .h = @floatFromInt(self.height),
         };
+    }
 
+    pub fn render(self: *Texture, x: f32, y: f32) void {
+        var dest = self.get_destination_rect(x, y);
         _ = sdl.SDL_RenderTexture(self.renderer, self.texture, null, &dest);
+    }
+
+    pub fn render_clipped(self: *Texture, x: f32, y: f32, clip: *sdl.SDL_FRect, width: f32, height: f32) void {
+        var dest = self.get_destination_rect(x, y);
+
+        // default to clipping rectangle
+        if (clip != null) {
+            dest.w = clip.w;
+            dest.h = clip.h;
+        }
+
+        // use sizes if given
+        if (width > 0) dest.w = width;
+        if (height > 0) dest.h = height;
+
+        _ = sdl.SDL_RenderTexture(self.renderer, self.texture, clip, &dest);
+    }
+
+    pub fn render_clipped_rotated(self: *Texture, x: f32, y: f32, clip: *sdl.SDL_FRect, width: f32, height: f32, angle: f64, centre: *sdl.SDL_FPoint, flip: sdl.SDL_FlipMode) void {
+        var dest = self.get_destination_rect(x, y);
+
+        // default to clipping rectangle
+        if (clip != null) {
+            dest.w = clip.w;
+            dest.h = clip.h;
+        }
+
+        // use sizes if given
+        if (width > 0) dest.w = width;
+        if (height > 0) dest.h = height;
+
+        _ = sdl.SDL_RenderTextureRotated(self.renderer, self.texture, clip, dest, angle, centre, flip);
     }
 };
